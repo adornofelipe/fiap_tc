5e8cc23475cc708782fe57683b1da771e28f41b3
Felipe <102001664+adornofelipe@users.noreply.github.com>
2024-01-08 23:58:54 -0300

    Update function.js

--- function.js
+++ function.js
@@ -1,8 +1,8 @@
 async function teste () {
-    const supabaseUrl = 'https://pklddrmlchsmwvopaqht.supabase.co'; // Substitua pelo URL do seu projeto Supabase
-    const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBrbGRkcm1sY2hzbXd2b3BhcWh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDM4NjgzOTcsImV4cCI6MjAxOTQ0NDM5N30.VhBhznFm6cLrGLeINrRD0nLHmXmYDmSGXbxOrb7-Nlc'; // Substitua pela sua chave do Supabase
+    const supabaseUrl = ''; // Substitua pelo URL do seu projeto Supabase
+    const supabaseKey = ''; // Substitua pela sua chave do Supabase
     
-    const table = 'cliente'; // Substitua pelo nome da sua tabela
+    const table = ''; // Substitua pelo nome da sua tabela
     
     fetch(`${supabaseUrl}/${table}`, {
       method: 'GET',
